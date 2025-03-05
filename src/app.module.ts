c50162daf73e7f191e4826d85609d0784ddb9feb
Nicolae Lupei <nicolae.lupei@acconomy.com>
2025-03-05 14:30:11 +0200

    feat: update to 1.0.34

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { CliModule } from '@qodalis/angular-cli';
-import { CliGuidModule } from '@qodalis/cli-guid';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CliModule } from "@qodalis/angular-cli";
+import { CliGuidModule } from "@qodalis/cli-guid";
 
 @NgModule({
   declarations: [],
-  imports: [BrowserModule, CliModule, CliGuidModule],
+  imports: [BrowserModule, BrowserAnimationsModule, CliModule, CliGuidModule],
   providers: [],
   bootstrap: [],
 })
